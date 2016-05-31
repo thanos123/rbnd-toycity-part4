12354bcd8342065b454fbec1cc6d9fa512479f82
Thanos Atskakanis <thanosatskakanis@gmail.com>
2016-05-31 17:42:40 +0100

    analyzable average

--- lib/analyzable.rb
+++ lib/analyzable.rb
@@ -1,3 +1,8 @@
 module Analyzable
-  # Your code goes here!
+
+  # find the average price of the passed products
+  def average_price(products)
+    price_sum = products.inject(0){ |sum, product| sum + product.price.to_f }
+    (price_sum / products.size).round(2)
+  end
 end
