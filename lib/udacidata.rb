a8b174fe2a92dff5b157d1cf93670101031a4170
Thanos Atskakanis <thanosatskakanis@gmail.com>
2016-05-31 11:12:24 +0100

    find_by_ methods

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -7,53 +7,11 @@ class Udacidata
   
   # creates a new object and saves it if it doesn't exist in the CSV file 
   def self.create(attributes={})
-    return self.new(attributes) if item_exists(attributes)
+    return self.new(attributes) if item_exists?(attributes)
     object = self.new(attributes)
-    CSV.open(@@data_path, "ab") do |csv|
-      csv << object_to_row(object)
-    end
+    save_to_csv(object)
     object
   end 
-
-  # turns the object into a CSV row, 
-  # placing the values in the correct order based on the headers of the CSV file
-  def self.object_to_row(object)
-    row = []
-    headers = get_headers
-    headers.each do |header|
-      # if the header has the name of the class then look for its "name" variable
-      var_name = header == object.class.name.downcase ? "name" : header
-      row << object.instance_variable_get("@#{var_name}")
-    end
-    row
-  end
-
-  # returns the headers of the CSV file
-  def self.get_headers  
-    CSV.read(@@data_path).first
-  end
-
-  # returns the data of the CSV file, minus the headers
-  def self.get_data
-    CSV.read(@@data_path).drop(1)
-  end
-
-  # combines the data and the headers into an array of hashes
-  def self.get_data_hashes
-    headers = get_headers
-    get_data.map {|a| Hash[ headers.map { |x| x.to_sym }.zip(a) ] }
-  end
-
-  # checks if an item with the passed attributes exists in the database
-  def self.item_exists(attributes)
-    return false if attributes == {}
-    get_data_hashes.each do |data_hash|
-      # remove the elements of the hash whose keys are not present in the attributes hash
-      filtered_hash = data_hash.select { |key,_| attributes.keys.include? key }
-      return true if filtered_hash == attributes
-    end
-    false
-  end
 
   # returns an array of all objects in the database
   def self.all
@@ -74,4 +32,97 @@ class Udacidata
     (n-all.length).times { self.create }
     return n == 1 ? all.last : all.last(n)
   end 
+
+  # finds a product by id, or creates it if it doesn't exist
+  def self.find(id)
+    item = all.find { |item| item.id == id }
+    return item == nil ? self.create({id: id}) : item
+  end
+
+  def self.destroy(id)
+    item = find(id)
+    delete_from_csv(item)
+  end
+
+  def self.method_missing(method_name, *arguments)
+    if method_name.to_s.start_with?('find_by_')
+      # get the substring after "find_by", which is what we are trying to find
+      field = method_name.to_s[8..-1]
+      return all.find { |item| item.instance_variable_get("@#{field}") == arguments[0] }
+    else
+      puts "No method named #{method_name}"
+      super
+    end
+  end
+
+  private
+
+  # turns the object into a CSV row, 
+  # placing the values in the correct order based on the headers of the CSV file
+  def self.object_to_row(object)
+    row = []
+    get_headers.each do |header|
+      header = header == self.name.downcase ? "name" : header
+      row << object.instance_variable_get("@#{header}").to_s
+    end
+    row
+  end
+
+  # returns the headers of the CSV file
+  def self.get_headers  
+    CSV.read(@@data_path).first
+  end
+
+  # returns the data of the CSV file, minus the headers
+  def self.get_data
+    CSV.read(@@data_path).drop(1)
+  end
+
+  # saves all data to the CSV file
+  def self.save_data(rows)
+    headers = get_headers
+    CSV.open(@@data_path, "wb") do |csv|
+      csv << headers
+      rows.each { |row| csv << row }
+    end
+  end
+
+  # combines the data and the headers into an array of hashes
+  def self.get_data_hashes
+    headers = get_headers
+    # if the header has the class name, change it to "name"
+    headers.map! do |header|
+      header == self.name.downcase ? "name" : header
+    end
+    get_data.map {|a| Hash[ headers.map { |x| x.to_sym }.zip(a) ] }
+  end
+
+  # checks if an item with the passed attributes exists in the database
+  def self.item_exists?(attributes)
+    return false if attributes == {}
+    get_data_hashes.each do |data_hash|
+      # remove the elements of the hash whose keys are not present in the attributes hash
+      filtered_hash = data_hash.select { |key,_| attributes.keys.include? key }
+      return true if filtered_hash == attributes
+    end
+    false
+  end
+
+  # saves a single entry to the CSV file
+  def self.save_to_csv(object)
+    CSV.open(@@data_path, "ab") do |csv|
+      csv << object_to_row(object)
+    end
+  end
+
+  # deletes a single object from the CSV
+  def self.delete_from_csv(object)
+    rows = get_data
+    rows.delete_if do |row|
+      object_to_row(object) == row
+    end
+    save_data(rows)
+    object
+  end
+
 end
